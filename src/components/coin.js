20c9112e616e38e2ab46eceda5b77708c0fa85b6
Abhishek <akanth1994@gmail.com>
2022-10-05 14:40:06 +0200

    merge to the code

--- src/components/coin.js
+++ src/components/coin.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 
 function CoinComponent({ cryptoId, cryptoName }) {
   const [tokenDetails, setTokenDetails] = useState(null);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const getPrice = () => {
-    setButtonDisabled(true);
+    setIsLoading(true);
     getPriceDetails(cryptoId).then((res) => {
       console.log(res);
       setTokenDetails({
@@ -16,6 +16,7 @@ function CoinComponent({ cryptoId, cryptoName }) {
           res.data[cryptoId].quote.USD.percent_change_24h
         ).toFixed(2),
       });
+      setIsLoading(false);
       return;
     });
   };
@@ -24,24 +25,26 @@ function CoinComponent({ cryptoId, cryptoName }) {
     <div>
       <div class=" bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
         <div class="px-2">
-          <div class="relative px-5 py-6 bg-slate-300 ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top flex-start space-x-6 grid-flow-row grid-cols-2 justify-between">
+          <div class="relative px-4 py-4 bg-slate-300 ring-1 ring-gray-900/5 rounded-lg leading-none ">
             {!tokenDetails ? (
-              <>
-                <div>
-                  <p class="text-slate-800 font-bold">{cryptoName}</p>
+              !isLoading ? (
+                <div class="flex flex-start justify-between">
+                  <div class="flex-2">
+                    <p class="text-slate-800 font-bold">{cryptoName}</p>
+                  </div>
+                  <div class="flex-2">
+                    <button class="float-right" onClick={getPrice}>
+                      Get {cryptoId} Price
+                    </button>
+                  </div>
                 </div>
-                <div>
-                  <button
-                    class="float-right"
-                    onClick={getPrice}
-                    disabled={buttonDisabled}
-                  >
-                    Get {cryptoId} Price
-                  </button>
+              ) : (
+                <div class="relative py-4">
+                  <div class="spin"></div>
                 </div>
-              </>
+              )
             ) : (
-              <>
+              <div class="flex flex-start justify-between">
                 <div class="font-bold">{tokenDetails.name}</div>
                 <div>{tokenDetails.symbol}</div>
                 <div>${tokenDetails.price}</div>
@@ -56,7 +59,7 @@ function CoinComponent({ cryptoId, cryptoName }) {
                     <sub>(past 24 hours)</sub>
                   </div>
                 )}
-              </>
+              </div>
             )}
           </div>
         </div>
